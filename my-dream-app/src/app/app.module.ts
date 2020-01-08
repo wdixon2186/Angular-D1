26caa82461f8655a6ecae48e39852a962c108556
wdixon2186 <53952397+wdixon2186@users.noreply.github.com>
2020-01-08 14:56:51 -0700

    working with binding and string interpolation

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
