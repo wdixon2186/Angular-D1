a0e73fd32ab16a0d10ff6cb1a7c61b3283f9c829
wdixon2186 <53952397+wdixon2186@users.noreply.github.com>
2020-01-09 16:55:01 -0700

    finished section 2

--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -9,21 +9,26 @@ export class ServersComponent implements OnInit {
 allowNewServer = false;
 serverCreationStatus = 'No server was created!';
 serverName = 'TestServer';
+serverCreated = false;
 allowNewUserName = false;
 userNameCreationStatus = 'No username was created'
 userName = '';
+servers = ['Testserver', 'Testserver 2']
 
-  // constructor() {
-  //   setTimeout(() => {
-  //     this.allowNewUserName = true;
-  //   }, 2000);
-  //  }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewUserName = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
 
   onCreateServer() {
   this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  this.serverCreated = true;
+  this.servers.push(this.serverName);
   }
   onUpdateServerName(event: any){
     this.serverName = (<HTMLInputElement>event.target).value;
